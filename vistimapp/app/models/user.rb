a5a41cef3db446d84319527e9906d02fafb19053
root <root@progra-bajo-nivel.gamerlive.cl>
2019-05-20 20:07:46 -0400

    Added Devise Gem and implemented custom view for login/logout

--- vistimapp/app/models/user.rb
+++ vistimapp/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-   has_many :complaints
-   has_many :comments
-   has_many :reports
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :complaints
+  has_many :comments
+  has_many :reports
 end
